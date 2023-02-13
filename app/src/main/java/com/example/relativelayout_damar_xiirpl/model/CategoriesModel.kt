0e4bd66d474ec875b219d37908097b628c5a9058
damar111 <muhamaddamar206@gmail.com>
2023-02-13 15:37:57 +0700

    menambahkan android networking dan adapter

---
+++ app/src/main/java/com/example/relativelayout_damar_xiirpl/model/CategoriesModel.kt
@@ -0,0 +1,13 @@
+package com.example.relativelayout_damar_xiirpl.model
+
+import java.io.Serializable
+
+class CategoriesModel : Serializable {
+    @JvmField
+    var strCategory :String? = null
+
+    @JvmField
+    var strCategoryThumb :String?=null
+
+    var strCategoryDescription :String?=null
+}
---
+++ app/src/main/java/com/example/relativelayout_damar_xiirpl/networking/Api.kt
@@ -0,0 +1,5 @@
+package com.example.relativelayout_damar_xiirpl.networking
+
+object Api {
+    var Categories = "https://www.themealdb.com/api/json/v1/1/categories.php"
+}
